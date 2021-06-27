dfe99d2bef722e0bb8483cc77c892f863308622e
AlexandreLemes91 <lemesalexandre91@gmail.com>
2021-06-27 23:40:10 +0200

    router - Home - Blog - 404

---
+++ resources/js/App copy.vue
@@ -0,0 +1,58 @@
+<template>
+
+    <div class="container">
+        <Header />
+
+        
+
+    </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+
+export default {
+    name: 'App',
+
+    components: {
+        Header,
+    },
+
+    
+}
+</script>
+
+<style lang='scss'>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.container{
+    margin: auto;
+    max-width: 1170px;
+}
+
+ul{
+    display: flex;
+    list-style: none;
+
+    li{
+        margin: 0 10px;
+    }
+}
+
+nav{
+    margin-top: 20px;
+
+    .active{
+        background-color: aquamarine;
+    }
+}
+
+button{
+    padding: 3px;
+}
+
+
+</style>
